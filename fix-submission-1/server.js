726499ed2f485ef7141edaf4f5ae16e5268f3f66
RedRabbit0130 <superworlddev0130@gmail.com>
2026-06-19 12:59:22 -0400

    Restart project with clean structure

--- fix-submission-1/server.js
+++ fix-submission-1/server.js
@@ -1,22 +1,24 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 3000;
-const APP_MESSAGE = process.env.APP_MESSAGE || "Hello from CI/CD";
+const APP_MESSAGE = process.env.APP_MESSAGE || "Hello DevOps Beginner";
 
 const server = http.createServer((req, res) => {
-        if(req.url === "/health"){
-                res.writeHead(200, {"Content-Type": "application/json"});
-                res.end(JSON.stringify({
-                        status:"ok",
-                        app: "simple-devops-app"
-                }));
-                return;
-        }
+  if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        status: "ok",
+        app: "fix-submission-1",
+      })
+    );
+    return;
+  }
 
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end(APP_MESSAGE);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(APP_MESSAGE);
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-        console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
